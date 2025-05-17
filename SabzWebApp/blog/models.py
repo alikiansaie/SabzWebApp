33f7a1da87fd8976733eec524ba9e7c89b86390f
alikiansaee <ali.kian.saii@gmail.com>
2025-05-17 11:13:02 +0330

    adding author field for db

--- SabzWebApp/blog/models.py
+++ SabzWebApp/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -8,8 +8,9 @@ class Post(models.Model):
         DRAFT = "DR", "Draft"
         PUBLISHED = "PB", "Published"
         REJECTED = "RJ", "Rejfected"
-
-
+    #relations
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
+    #data fields
     title= models.CharField(max_length=250)
     description = models.TextField()
     slug = models.SlugField(max_length=250)
@@ -17,6 +18,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    #choice fields
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     class Meta:
         ordering = ['-publish']
