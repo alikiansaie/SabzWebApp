2a0770d7e1babcb48432972db0a90bee8522f99e
alikiansaee <ali.kian.saii@gmail.com>
2025-05-17 11:06:09 +0330

    add requirements file / add status field to models

--- SabzWebApp/blog/models.py
+++ SabzWebApp/blog/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DR", "Draft"
+        PUBLISHED = "PB", "Published"
+        REJECTED = "RJ", "Rejfected"
+
+
     title= models.CharField(max_length=250)
     description = models.TextField()
     slug = models.SlugField(max_length=250)
@@ -9,7 +17,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     class Meta:
         ordering = ['-publish']
         indexes = [
